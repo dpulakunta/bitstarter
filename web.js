601f61b0795440e4ebb3fd605072fb7ad113fd41
dpulakunta <cool555cigar@gmail.com>
2013-07-13 04:16:48 +0000

    Updated file web.js

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
-
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  var fs = require('fs');
+  var inputfile = "index.html"
+  var buf = new Buffer(256000);
+  buf = fs.readFileSync(inputfile);
+  var inputString = buf.toString();
+  response.send(inputString);
 });
 
 var port = process.env.PORT || 5000;
